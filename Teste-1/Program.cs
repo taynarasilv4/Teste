d60676e08730b6b16c7eaddaebcbacf6b2a26423
Taynara <taynarasilvarodriguesr@gmail.com>
2023-05-30 15:03:06 -0400

    Inicio do projeto de divisão de dois numeros

---
+++ Teste-1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Informe o primeiro valor!");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo valor!");
+int b = int.Parse(Console.ReadLine());
+
+int resultado = a / b;
+Console.WriteLine($"Resultado = {resultado}");
