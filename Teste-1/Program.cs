62b68d1fe6aa171edc02e39cfaed510b64d587a0
Taynara <taynarasilvarodriguesr@gmail.com>
2023-05-30 15:28:55 -0400

    Atualizaçao do projeto de divisão

--- Teste-1/Program.cs
+++ Teste-1/Program.cs
@@ -1,7 +1,12 @@
-﻿Console.WriteLine("Informe o primeiro valor!");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Informe o segundo valor!");
-int b = int.Parse(Console.ReadLine());
-
-int resultado = a / b;
-Console.WriteLine($"Resultado = {resultado}");
+﻿try
+{
+    Console.WriteLine("Informe o primeiro valor!");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe o segundo valor!");
+    int b = int.Parse(Console.ReadLine());
+    int resultado = a / b;
+    Console.WriteLine($"Resultado = {resultado}");
+}catch(Exception ex)
+{
+    Console.WriteLine("Algo de errado não está certo!");
+}
